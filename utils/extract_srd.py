1c539e570358ba8994f22e7eb2380b92421af0be
LS-2ednar <lukas_schaub@gmx.ch>
2025-09-14 08:13:05 +0200

    finished helper function to extract domaincards from the SRD pdf

--- utils/extract_srd.py
+++ utils/extract_srd.py
@@ -25,19 +25,77 @@ def extract_domain_cards(srd):
     #Firstpage is different then following pages therefore considered as a special case.
     domaincard_page = reader.pages[page_count].extract_text().split("Domain Card reference\n")[1]
     while page_count < len(reader.pages)-1:
-        print(domaincard_page) ### <---- CONTINUE HERE Extract Data from the page and repeat until the page_refernce_count is reached###
 
-        """
-        Pseudo Code:
-        1. Ignore " " lines
-        2. Ignore "Domain" Lines
-        3. All CAPs -> DOMAIN CARD NAME
-        4. If "Level" in Line -> Level, Domain & Type
-        5. Unitl Next all CAPs -> Text for this Domain Card 
-        """
+        def tnum_to_number(tnum):
+            #replacement numbers 
+            word_to_num = {
+            "zero": 0,
+            "one": 1,
+            "two": 2,
+            "three": 3,
+            "four": 4,
+            "five": 5,
+            "six": 6,
+            "seven": 7,
+            "eight": 8,
+            "nine": 9,
+            }
+            number = ""
+            for tnum in tnum.split("/"):
+                if tnum != "":
+                    number += str(word_to_num[tnum.replace(".tnum","")])
+            return number
+
+        def card_text_refresher(text):
+
+            cleaned_numbers = text.replace("/one.tnum","1").replace("/two.tnum","2").replace("/three.tnum","3").replace("/four.tnum","4").replace("/five.tnum","5").replace("/six.tnum","6").replace("/seven.tnum","7").replace("/eight.tnum","8").replace("/nine.tnum","9").replace("/zero.tnum","0")
+            cleaned_punctuations = cleaned_numbers.replace("/comma.tab",",").replace("/period.tab",".")
+            cleaned_text = cleaned_punctuations.replace("/uni00A0"," ")
+            return cleaned_text
+
+        CARD_TEXT = ""
+        for line in domaincard_page.split("\n"):
+
+            #lines to ignore
+            if "SRD" in line or "DOMAIN" in line:
+                print("IGNORED LINE -> DOMAIN or SRD")
+                continue
+
+            #identify domaincard name
+            if line.isupper():
+                print(f"CARDTEXT:\n{card_text_refresher(CARD_TEXT)}")
+                print(f"DOMAINCARD NAME LINE:{line.capitalize()}")
+                CARDNAME = line.capitalize()
+                CARD_TEXT = ""
+                continue
+
+            # identify cardlevel, domain and type
+            if "Level /" in line:
+                LEVEL = tnum_to_number(line.split(" ")[1])
+                DOMAIN = line.split(".tnum")[-1].split(" ")[1]
+                TYPE = line.split(".tnum")[-1].split(" ")[2]
+                print(f"DOMAINCARD INFORMATION LINE: LEVEL={LEVEL}, DOMAIN={DOMAIN}, TYPE={TYPE}")
+                continue
+
+            if "Recall Cost:" in line:
+                RECALL = tnum_to_number(line.split("Recall Cost:")[1].strip())
+                print(f"Recall Cost={RECALL}")
+                continue
+
+            # update cardtext
+            CARD_TEXT += line 
+
+            # hacky way of writing the contents to the dict -> is updated multiple times
+            if DOMAIN not in domain_cards:
+                domain_cards[DOMAIN]={}
+
+            domain_cards[DOMAIN][CARDNAME] = [LEVEL, RECALL, TYPE, card_text_refresher(CARD_TEXT)]
 
         page_count += 1
         domaincard_page = reader.pages[page_count].extract_text()
+
+    print(domain_cards)
+    return domain_cards
 
 def read_srd_contents(srd):
     # extract the contents of the SRD
@@ -53,32 +111,32 @@ def read_srd_contents(srd):
             #print(" ".join(line.replace(".","").split()))
             tnum_to_int(" ".join(line.replace(".","").split()))
 
-def tnum_to_int(tnum):
-    #replace all wrong 
-    word_to_num = {
-    "zero": 0,
-    "one": 1,
-    "two": 2,
-    "three": 3,
-    "four": 4,
-    "five": 5,
-    "six": 6,
-    "seven": 7,
-    "eight": 8,
-    "nine": 9,
-    }
+    def tnum_to_int(tnum):
+        #replacement numbers 
+        word_to_num = {
+        "zero": 0,
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9,
+        }
 
-    parts = tnum.split("/")
-    section = parts[0]
-    numbers = parts[1:]
-    str_numbers = ""
-    for number in numbers:
-        if not "tnum" in number:
-            section += number
-            continue
-        str_numbers+=str((word_to_num[number.replace("tnumINTRODUCTION","").replace("tnum","")]))
+        parts = tnum.split("/")
+        section = parts[0]
+        numbers = parts[1:]
+        str_numbers = ""
+        for number in numbers:
+            if not "tnum" in number:
+                section += number
+                continue
+            str_numbers+=str((word_to_num[number.replace("tnumINTRODUCTION","").replace("tnum","")]))
 
-    print(section, str_numbers)
+        print(section, str_numbers)
 
 def extract_srd():
     srd = find_srd()
