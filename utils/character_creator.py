867be969b70f51ee279a8f159b65a444e9db5e5e
LS-2ednar <lukas_schaub@gmx.ch>
2025-11-08 18:42:32 +0100

    working on character creation functionality

--- utils/character_creator.py
+++ utils/character_creator.py
@@ -1,6 +1,25 @@
 from fillpdf import fillpdfs
 
+def new_char(char_class=None):
 
+    #1 check files for identical naming
+    #2 if identical get from source everytime -> otherwise do it yourself using sedja
+    #3 Use character creation process to generate the first bases of the character
+
+    form_fields = fillpdfs.get_form_fields("Bard.pdf").keys()
+    print(form_fields)
+    for field in form_fields:
+        if "_" in field:
+            continue
+        print(field)
+
+    pass
+
+
+
+
+#Example
+"""
 form_fields = fillpdfs.get_form_fields("character_sheet.pdf").keys()
 
 for field in form_fields:
@@ -21,3 +40,6 @@ data = {
 print(form_fields)
 
 fillpdfs.write_fillable_pdf("character_sheet.pdf","test_char.pdf", data)
+"""
+if __name__ == "__main__":
+    new_char()
